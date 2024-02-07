60573aeadfd29649c3d622e10bb17b09b5f41e9d
ygursu <yasemingrs@gmail.com>
2024-02-08 01:50:27 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,42 @@
+﻿
+private static double PisagorHesapla(int sayi1, int sayi2)
+{
+
+    return Math.Sqrt(sayi1 * sayi1 + sayi2 * sayi2);
+
+}
+
+
+
+
+static void Main(string[] args)
+{
+    Console.Write("Birinci Dik Kenarı Girin");
+
+    int s1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("İkinci Dik Kenarı Girin");
+
+    int s2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Hipotenüs Kenarı Uzunluğu:" + PisagorHesapla(s1, s2));
+
+    Console.ReadKey();
+}
+
+
+static void Main(string[] args)
+{
+    Console.Write("Birinci Dik Kenarı Girin");
+
+    int s1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("İkinci Dik Kenarı Girin");
+
+    int s2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Hipotenüs Kenarı Uzunluğu:" + PisagorHesapla(s1, s2));
+
+    Console.ReadKey();
+}
+
